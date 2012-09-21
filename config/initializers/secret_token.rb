c657dce90e43db0132d2c1b4e7aa78e673855dd4
Andrew Knight <apk@Andrews-MacBook-Pro.local>
2012-09-21 05:21:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Apknight::Application.config.secret_token = '92595ad19c209b300d353413c10b4114ba2240d636b395fbe84ed12072475bd1594ca7b11683fc01c47f6557792ffc29749264e1bcf0f1bb6960620ee52fdeed'
